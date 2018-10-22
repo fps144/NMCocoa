65e4a3ffb29ffa8840d68c92f5b8bf728e02b93c
南门 <yu.song@cisdi.com.cn>
2018-10-22 19:18:58 +0800

    feat: attributeStringWithString

--- NMCocoa/Cocoa/Category/Foundation/NSAttributedString+NMCocoa.h
+++ NMCocoa/Cocoa/Category/Foundation/NSAttributedString+NMCocoa.h
@@ -7,10 +7,13 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NSAttributedString (NMCocoa)
+
++ (NSAttributedString *)attributeStringWithString:(NSString *)string font:(UIFont *)font color:(UIColor *)color;
 
 @end
 
