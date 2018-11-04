915e36b987ccac665e45ca1ca43d7dc862a6934a
南门 <Yu.Song@cisdi.com.cn>
2018-11-04 19:32:13 +0800

    feat: Add NSTimer category

---
+++ NMCocoa/Cocoa/Category/Foundation/NMCocoa.h
@@ -0,0 +1,29 @@
+//
+//  NMCocoa.h
+//  NMCocoa
+//
+//  Created by Songyu on 2018/11/4.
+//  Copyright © 2018 南门. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NMCocoa : NSTimer
+
+/**
+ 一个不会引发循环引用的timer。（必要时需搭配weakSelf和strongSelf使用）
+
+ @param interval 时间间隔
+ @param repeats  是否重复
+ @param block    要执行的操作block
+ @return         timer对象
+ */
++ (NSTimer *)nm_scheduledTimerWithTimeInterval:(NSTimeInterval)interval
+                                       repeats:(BOOL)repeats
+                                         block:(void(^)(void))block;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NMCocoa/Cocoa/Category/Foundation/NMCocoa.m
@@ -0,0 +1,22 @@
+//
+//  NMCocoa.m
+//  NMCocoa
+//
+//  Created by Songyu on 2018/11/4.
+//  Copyright © 2018 南门. All rights reserved.
+//
+
+#import "NMCocoa.h"
+
+@implementation NMCocoa
+
++ (NSTimer *)nm_scheduledTimerWithTimeInterval:(NSTimeInterval)interval repeats:(BOOL)repeats block:(void (^)(void))block {
+    return [self scheduledTimerWithTimeInterval:interval target:self selector:@selector(nm_scheduledTimerBlock:) userInfo:[block copy] repeats:repeats];
+}
+
++ (void)nm_scheduledTimerBlock:(NSTimer *)timer {
+    dispatch_block_t block = timer.userInfo;
+    if (block) block();
+}
+
+@end
